1874b72fbd6fedd92151a665f274d13d7381acf9
piebin <76805688+piebin@users.noreply.github.com>
2021-04-03 03:21:35 +0900

    1일 시작

--- app/src/main/java/com/hansung/android/teamproject/MonthViewActivity.java
+++ app/src/main/java/com/hansung/android/teamproject/MonthViewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -33,7 +34,7 @@ public class MonthViewActivity extends AppCompatActivity {
         gridview.setAdapter(adapter); //adapter 연결
 
         TextView yearmonthTv = findViewById(R.id.yearmonth); //yearmonth 텍스트뷰 객체생성
-        yearmonthTv.setText(mCal.get(Calendar.YEAR) + "년" +mCal.get(Calendar.MONTH)+"월");
+        yearmonthTv.setText(mCal.get(Calendar.YEAR) + "년" +(mCal.get(Calendar.MONTH)+1)+"월");
 
     }
 
@@ -42,6 +43,10 @@ public class MonthViewActivity extends AppCompatActivity {
         daylist = new ArrayList();
         mCal.set(Calendar.MONTH, month-1); //calendar 객체의 월 설정
 
+        for(int i=0; i<=mCal.get(Calendar.DAY_OF_MONTH); i++)
+        {
+            daylist.add("");
+        }
         for(int i=0; i<mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) { //달의 마지막 날짜까지 반복
             daylist.add(i+1); //리스트에 추가
         }
