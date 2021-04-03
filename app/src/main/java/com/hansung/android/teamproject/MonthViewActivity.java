1dbe9cc58ac556f6a10a24840885e55fcaa56a6b
piebin <76805688+piebin@users.noreply.github.com>
2021-04-03 15:09:41 +0900

    Toast메시지 출력
    제컴 toast출력이 안돼서 println으로 기록남겨서 확인했어요. 혹시 toast잘 작동되는지 확인부탁드려요.

--- app/src/main/java/com/hansung/android/teamproject/MonthViewActivity.java
+++ app/src/main/java/com/hansung/android/teamproject/MonthViewActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -30,20 +32,32 @@ public class MonthViewActivity extends AppCompatActivity {
         mCal = Calendar.getInstance(); //Calendar 객체 생성
         int year= mCal.get(Calendar.YEAR);
         int month = mCal.get(Calendar.MONTH);
-        int day = 1;
-        setDate(year, month, day); //setDate method에 파라미터로 월 전달
+        int date=1;
+        setDate(year, month, date); //setDate method에 파라미터로 월 전달
         gridview = findViewById(R.id.gridview);
         adapter = new Adapter(this, daylist);
         gridview.setAdapter(adapter); //adapter 연결
 
         TextView yearmonthTv = findViewById(R.id.yearmonth); //yearmonth 텍스트뷰 객체생성
         yearmonthTv.setText(year + "년" +(month+1)+"월");
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MonthViewActivity.this, year+"." +(month+1)+"."+(position-mCal.get(Calendar.DAY_OF_WEEK)+2),
+                        Toast.LENGTH_SHORT).show();
+                System.out.println(year+"." + (month+1)+"."+(position-mCal.get(Calendar.DAY_OF_WEEK)+2)); //출력 확인용
+
+            }
+        });
+
     }
 
-    public void setDate(int year, int month, int day) {
+
+    public void setDate(int year, int month, int date) {
 
         daylist = new ArrayList();
-        mCal.set(year, month, day); //calendar 객체의 월 설정
+        mCal.set(year, month, date); //calendar 객체의 월 설정
 
         for(int i=1; i<mCal.get(Calendar.DAY_OF_WEEK); i++)
         {
